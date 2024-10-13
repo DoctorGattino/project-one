f79f7330fcf0af713d16b53fd428571b25dce53a
Boris Zalesskii <Doctorgattino@gmail.com>
2024-10-14 01:07:37 +0300

    чтобы красным не горело

---
+++ tusk_1.5/js/script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    slidesPerView: "auto",
+  });
+});
+
+
+/* 	document.addEventListener("DOMContentLoaded", function(){
+  var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+		slidesPerView: 1.2,
+  });
+}); */
