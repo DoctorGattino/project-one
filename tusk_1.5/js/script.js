06ab8419041ae6b8899cb81b0e398446a67abcb1
Boris Zalesskii <Doctorgattino@gmail.com>
2024-10-14 02:11:50 +0300

    123

--- tusk_1.5/js/script.js
+++ tusk_1.5/js/script.js
@@ -1,3 +1,4 @@
+if (window.matchMedia('(max-width: 767px)').matches) {
 document.addEventListener("DOMContentLoaded", function() {
 const swiper = new Swiper('.swiper', {
     // Optional parameters
@@ -12,15 +13,6 @@ const swiper = new Swiper('.swiper', {
     spaceBetween: 16,
   });
 });
+}
 
 
-/* 	document.addEventListener("DOMContentLoaded", function(){
-  var swiper = new Swiper('.swiper-container', {
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-		slidesPerView: 1.2,
-  });
-}); */
