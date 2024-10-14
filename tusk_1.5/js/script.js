a1d07613b9f9cd4c0a0219be271b9c6578718ba4
Boris Zalesskii <Doctorgattino@gmail.com>
2024-10-14 13:23:38 +0300

    js logic

--- tusk_1.5/js/script.js
+++ tusk_1.5/js/script.js
@@ -14,3 +14,29 @@ const swiper = new Swiper('.swiper', {
   });
 });
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const showButton = document.getElementById('showButton');
+    const brandsContainer = document.querySelector('.brands');
+  
+    let isHidden = false;
+  
+    showButton.addEventListener('click', function() {
+      isHidden = !isHidden;
+  
+      if (isHidden) {
+        // Показываем бренды
+        brandsContainer.style.height = '380px';
+        showButton.textContent = 'Скрыть';
+        showButton.classList.remove('main__buttons--show-all');
+        showButton.classList.add('main__buttons--hidden');
+      } else {
+        // Скрываем бренды
+
+        brandsContainer.style.height = '190px';
+        showButton.textContent = 'Показать все';
+        showButton.classList.remove('main__buttons--hidden');
+        showButton.classList.add('main__buttons--show-all');
+      }
+    });
+  });
