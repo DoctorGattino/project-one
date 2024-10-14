a6bff0b925bba615f99d1b0896c6ce50a807aa67
Boris Zalesskii <Doctorgattino@gmail.com>
2024-10-14 17:00:37 +0300

    final?

--- tusk_1.5/js/script.js
+++ tusk_1.5/js/script.js
@@ -1,49 +1,51 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 if (window.matchMedia('(max-width: 767px)').matches) {
-document.addEventListener("DOMContentLoaded", function() {
-const swiper = new Swiper('.swiper', {
+  document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.swiper', {
     // Optional parameters
-    loop: true,
-      slidesPerView: "auto",
-    spaceBetween: 1,
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    breakpoints: {
+      loop: true,
+      slidesPerView: 'auto',
+      spaceBetween: 1,
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      breakpoints: {
         769: {
-            toggle: false,
-            loop: false,
-            spaceBetween: 'auto',
-            centeredSlides: false
-        }
+          toggle: false,
+          loop: false,
+          spaceBetween: 'auto',
+          centeredSlides: false,
+        },
+      },
+    });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const showButton = document.getElementById('showButton');
+  const brandsContainer = document.querySelector('.brands');
+
+  let isHidden = false;
+
+  showButton.addEventListener('click', () => {
+    isHidden = !isHidden;
+
+    if (isHidden) {
+      // Показываем бренды
+      brandsContainer.style.height = '380px';
+      showButton.textContent = 'Скрыть';
+      showButton.classList.remove('main__buttons--show-all');
+      showButton.classList.add('main__buttons--hidden');
+    } else {
+      // Скрываем бренды
+
+      brandsContainer.style.height = '190px';
+      showButton.textContent = 'Показать все';
+      showButton.classList.remove('main__buttons--hidden');
+      showButton.classList.add('main__buttons--show-all');
     }
   });
 });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    const showButton = document.getElementById('showButton');
-    const brandsContainer = document.querySelector('.brands');
-  
-    let isHidden = false;
-  
-    showButton.addEventListener('click', function() {
-      isHidden = !isHidden;
-  
-      if (isHidden) {
-        // Показываем бренды
-        brandsContainer.style.height = '380px';
-        showButton.textContent = 'Скрыть';
-        showButton.classList.remove('main__buttons--show-all');
-        showButton.classList.add('main__buttons--hidden');
-      } else {
-        // Скрываем бренды
-
-        brandsContainer.style.height = '190px';
-        showButton.textContent = 'Показать все';
-        showButton.classList.remove('main__buttons--hidden');
-        showButton.classList.add('main__buttons--show-all');
-      }
-    });
-  });
